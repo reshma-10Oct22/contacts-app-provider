639c868a301342d388387d0b4c313b797d0b9a17
reshma-10Oct22 <reshma.hs@zohocorp.com>
2023-04-18 12:32:43 +0530

    Update fav_contacts_screen.dart

--- lib/screens/fav_contacts_screen.dart
+++ lib/screens/fav_contacts_screen.dart
@@ -14,7 +14,6 @@ class FavContactsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    print(favContactsList.length);
     return favContactsList.isEmpty
         ? const Center(
             child: Text("No contacts added in favourites"),
